9205b80c05f5fa591ea6695c60ab20b17136f74e
cavonLucky <425247833@qq.com>
2022-02-14 22:43:04 +0800

    react中的响应式设计思想和事件绑定

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class TodoList extends React.Component {
+
+  constructor(props) {
+    super(props); // 指向 Component
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+  }
+
+  handleInputChange(e) {
+    this.setState({ inputValue: e.target.value });
+  }
+
   render() {
+
+    const { inputValue } = this.state;
     return (
       <React.Fragment>
         <div>
-          <input />
+          <input value={inputValue} onChange={this.handleInputChange.bind(this)} />
           <button>提交</button>
         </div>
         <ul>
