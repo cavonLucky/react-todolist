f98012069003d791c8ce00ac18edff5a9566c16d
cavonLucky <425247833@qq.com>
2022-02-16 15:15:42 +0800

    Todolist 代码优化

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,51 +4,65 @@ import './style.css';
 class TodoList extends React.Component {
 
   constructor(props) {
-    super(props); // 指向 Component
+    super(props);
     this.state = {
       inputValue: '',
       list: []
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
   handleInputChange(e) {
-    this.setState({ inputValue: e.target.value });
+    // es6 简写
+    this.setState(() => ({
+      inputValue: e.target.value
+    }));
   }
 
   handleBtnClick() {
-    this.setState({ list: [...this.state.list, this.state.inputValue], inputValue: '' });
+    this.setState((prevState) => ({
+      list: [...prevState.list, prevState.inputValue],
+      inputValue: ''
+    }));
   }
 
   handleItemDelete(index) {
-    // immutable: state 不允许我们做任何的改变，如果要改变拷贝一个副本 
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({ list });
+    this.setState((prevState) => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return { list };
+    });
+  }
+
+  getTodoItem() {
+    return (
+      this.state.list.map((item, index) => {
+        return (
+          <TodoItem
+            key={index}
+            content={item}
+            index={index}
+            deleteItem={this.handleItemDelete}
+          />
+        )
+      })
+    )
   }
 
   render() {
 
-    const { inputValue, list } = this.state;
+    const { inputValue } = this.state;
     return (
       <React.Fragment>
         <div>
           <label htmlFor="insertArea">输入内容</label>
-          <input id="insertArea" className="input" value={inputValue} onChange={this.handleInputChange.bind(this)} />
-          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+          <input id="insertArea" className="input" value={inputValue} onChange={this.handleInputChange} />
+          <button onClick={this.handleBtnClick}>提交</button>
         </div>
         <ul>
-          {
-            list.map((item, index) => {
-              return (
-                <TodoItem
-                  key={index}
-                  content={item}
-                  index={index}
-                  deleteItem={this.handleItemDelete.bind(this)}
-                />
-              )
-            })
-          }
+          {this.getTodoItem()}
         </ul>
       </React.Fragment>
     )
