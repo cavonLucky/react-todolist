806746c7930c5a78a12464a9efde3347fb89f33f
cavonLucky <425247833@qq.com>
2022-02-17 17:23:49 +0800

    PropTypes与DefaultProps的应用

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 class TodoItem extends React.Component {
   constructor(props) {
@@ -6,14 +7,27 @@ class TodoItem extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
-    this.props.deleteItem(this.props.index); 
+    this.props.deleteItem(this.props.index);
   }
   render() {
-    const { content } = this.props;
+    const { content, test } = this.props;
     return (
-      <div onClick={this.handleClick}>{content}</div>
+      <div onClick={this.handleClick}>{test} - {content}</div>
     )
   }
+}
+
+// 做属性类型的强校验
+TodoItem.propTypes = {
+  content: PropTypes.string,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number,
+  test: PropTypes.string.isRequired 
+}
+
+// 加入 test 没填就给 test 一个默认值
+TodoItem.defaultProps = {
+  test: "hello world"
 }
 
 export default TodoItem;
