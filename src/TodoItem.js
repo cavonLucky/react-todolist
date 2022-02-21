70df4588a9440d20f454d16f10bd2417a5d9174e
cavonLucky <425247833@qq.com>
2022-02-21 17:47:14 +0800

    React生命周期函数的使用场景

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -12,46 +12,30 @@ class TodoItem extends React.Component {
     this.props.deleteItem(this.props.index);
   }
 
-  /**
-   * 1. 当一个组件要从父组件接收参数（条件）
-   * // 2. 只要父组件的 render 函数被重新执行了，子组件的这个生命周期函数就会被执行（执行时刻）
-   * 如果这个组件第一次存在于父组件中，不会执行
-   * 如果这个组件之前已经存在于父组件中，才会执行
-   */
-  componentWillReceiveProps() {
-    console.log('child componentWillReceiveProps');
-  }
-
-  // 当这个组件即将被从页面中剔除的时候，会被执行
-  componentWillUnmount() {
-    console.log('child componentWillUnmount');
+  // setState 异步函数 可以把多次数据的改变结合成一次来做 降低虚拟 dom 的刷新频率
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.content !== this.props.content) {
+       return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
     console.log('child render');
-    const { content, test } = this.props;
+    const { content } = this.props;
     return (
-      // JSX -> JS 对象 -> 真实的 DOM
-      <div onClick={this.handleClick}>{test} - {content}</div>
-      // 更倾向一个 react 的底层的结构, JSX -> createElement -> 虚拟 dom(JS 对象) -> 真实的 DOM
-      // React.createElement('div', {}, 'item');
+      <div onClick={this.handleClick}>{content}</div>
     )
   }
 }
 
 // 做属性类型的强校验
 TodoItem.propTypes = {
-  // content: PropTypes.string,
   // number || string
   content: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   deleteItem: PropTypes.func,
   index: PropTypes.number,
-  test: PropTypes.string.isRequired 
-}
-
-// 加入 test 没填就给 test 一个默认值
-TodoItem.defaultProps = {
-  test: "hello world"
 }
 
 export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios"; 
 import TodoItem from "./TodoItem";
 import "./style.css";
 class TodoList extends React.Component {
@@ -15,9 +16,9 @@ class TodoList extends React.Component {
     this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
-  handleInputChange() {
+  handleInputChange(e) {
     this.setState(() => ({
-      inputValue: this.input.value
+      inputValue: e.target.value
     }));
   }
 
@@ -25,12 +26,7 @@ class TodoList extends React.Component {
     this.setState((prevState) => ({
       list: [...prevState.list, prevState.inputValue],
       inputValue: ''
-    }), () => {
-      // 由于 setState 是异步，第二个参数是回调函数，所以此时直接操作的真实的dom结果正确 
-      console.log(this.ul.querySelectorAll('div').length);
-    });
-    // 直接操作真实的 dom 结果不对，ref 导致的，所以不建议、也尽量不使用 ref 
-    // console.log(this.ul.querySelectorAll('div').length); // 从 0 开始，0 1 2... 
+    }));
   }
 
   handleItemDelete(index) {
@@ -56,40 +52,15 @@ class TodoList extends React.Component {
     )
   }
 
-  // 在组件即将被挂载到页面的时刻 自动执行
-  componentWillMount() {
-    console.log('componentWillMount ');
-  }
-
-  // 组件在被挂载到页面之后自动被执行
   componentDidMount() {
-    console.log('componentDidMount');
+    axios.get('/api/todolist').then(() => {
+      alert('success');
+    }).catch(() => {
+      alert('error');
+    });
   }
-
-  // 组件被更新之前，他会被自动执行
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate');
-    // 你的组件需要更新吗：
-    return true;
-    // return false;
-  }
-
-  // 组件被更新之前，它会自动执行，但是他在shouldComponentUpdate之后
-  // 如果 shouldComponentUpdate 返回true它会执行
-  // 如果返回 false 这个函数就不会被执行了
-  componentWillUpdate() {
-    console.log('componentWillUpdate');
-  }
-
-  // 组件更新完成之后，它会被执行
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-
-  
 
   render() {
-    console.log('parent render ');
     const { inputValue } = this.state;
     return (
       <React.Fragment>
@@ -100,11 +71,10 @@ class TodoList extends React.Component {
             className="input"
             value={inputValue}
             onChange={this.handleInputChange}
-            ref={input => this.input = input}
           />
           <button onClick={this.handleBtnClick}>提交</button>
         </div>
-        <ul ref={ul => this.ul = ul}>
+        <ul>
           {this.getTodoItem()}
         </ul>
       </React.Fragment>
