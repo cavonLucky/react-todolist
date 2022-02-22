696138f5c55097903bba6bed77bcbe0a62c1fb99
cavonLucky <425247833@qq.com>
2022-02-22 11:01:39 +0800

    react中使用css动画效果

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React from "react";
+import "./style.css";
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true
+    }
+    this.handleToggole = this.handleToggole.bind(this);
+  }
+
+  handleToggole() {
+    let show = this.state.show ? false : true;
+    this.setState({ show });
+  }
+
+  render() {
+    const { show } = this.state;
+    return (
+      <React.Fragment>
+        <div className={show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.handleToggole}>toggole</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import TodoItem from "./TodoItem";
-import "./style.css";
 class TodoList extends React.Component {
 
   constructor(props) {
